9c0994125f3945b34b1b64382183530797607718
punn1035 <punnipa602.41@gmail.com>
2018-07-06 01:20:41 +0700

    edit

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,4 +1,8 @@
 class Organization < ApplicationRecord
     has_many :users
     has_many :projects
+
+    validates :name, :presence => true
+
 end
+
--- app/models/project.rb
+++ app/models/project.rb
@@ -1,4 +1,5 @@
 class Project < ApplicationRecord
     has_many :user_projects
     belongs_to :organization
+    has_many :users, through: :user_projects 
 end
